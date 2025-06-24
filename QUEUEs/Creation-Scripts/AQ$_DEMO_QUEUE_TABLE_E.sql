419dd01c86e2f94c1239744b343fbfae6ce7d28f
lsaperi <lsaperi>
2025-06-24 16:19:02 -0300

    fdsfds; TaskID: fdsds

--- QUEUEs/Creation-Scripts/AQ$_DEMO_QUEUE_TABLE_E.sql
+++
@@ -1,8 +0,0 @@
-BEGIN DBMS_AQADM.CREATE_QUEUE(
-     Queue_name          => 'AQ$_DEMO_QUEUE_TABLE_E',
-     Queue_table         => 'DEMO_QUEUE_TABLE',
-     Queue_type          =>  1,
-     dependency_tracking =>  FALSE,
-     comment             => 'exception queue');
-  END;
-
